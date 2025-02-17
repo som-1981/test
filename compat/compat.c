d277c783238c1956af0fc39e1b365c96358564d3
som-1981 <45190191+som-1981@users.noreply.github.com>
2025-02-17 22:29:59 +0530

    Update compat.c

--- compat/compat.c
+++ compat/compat.c
@@ -35,7 +35,7 @@ pthread_mutex_timedlock(pthread_mutex_t *mutex, const struct timespec *abstime)
     int64_t nsec_diff;
     struct timespec cur, dur;
     int rc;
-
+    int time_period = 0;
     /* try to acquire the lock and, if we fail, sleep for 5ms. */
     while ((rc = pthread_mutex_trylock(mutex)) == EBUSY) {
         /* get time */
